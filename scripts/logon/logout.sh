a7768c19f824a35da9970ec793367a7e2a3fdeda
Erik <eghijs@gmail.com>
2019-10-22 07:36:31 -0300

    Update logout.sh

--- scripts/logon/logout.sh
+++ scripts/logon/logout.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# Script de logout estações linux mint em rede.
 USERS=$(who |grep tty |awk '{print $1}')
 
 # Localizar o usuario pela UID, depois deleta a pasta do usuario
+#awk -F':' '$3 > 1001 {print $1}' /etc/passwd | xargs -n1 rm -rf !(.scripts) <-- este comando exclui tudo exeto o diretorio .scripts
 awk -F':' '$3 > 1001 {print $1}' /etc/passwd | xargs -n1 rm -rf !(.scripts)
 
 # Copiar diretorio base para novo usuario
