f7ea78a705eb0af24b44b70e1642c814732645fc
Erik <eghijs@gmail.com>
2019-10-22 17:54:29 -0300

    Update logout.sh

--- scripts/logon/logout.sh
+++ scripts/logon/logout.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 # Script de logout estações linux mint em rede.
-USERS=$(who |grep tty |awk '{print $1}')
+L_USER=$(who |grep tty |awk '{print $1}')
 
 # Localizar o usuario pela UID, depois deleta a pasta do usuario
 #awk -F':' '$3 > 1001 {print $1}' /etc/passwd | xargs -n1 rm -rf !(.scripts) <-- este comando exclui tudo exeto o diretorio .scripts
-awk -F':' '$3 > 1001 {print $1}' /etc/passwd | xargs -n1 rm -rf !(.scripts)
+#awk -F':' '$3 > 1001 {print $1}' /etc/passwd | xargs -n1 rm -rf !(.scripts)
+rm -rf /home/$L_USER/*
 
 # Copiar diretorio base para novo usuario
-cp -R /etc/skel /home/$USERS
+cp -R /etc/skel /home/$L_USER
+
+# Recriar pasta do usario no diretorio home
+mkhomedir_helper username
 
 # Criando diretórios padrão
 xdg-user-dirs-update
 
 # Define permissao de usuario e grupo
-chown -R $USERS:$USERS /home/$USERS
+chown -R L_USER:$L_USER /home/L_USER
 
 exit 0
+
+if [ "$(id -u)" = "1000" ]; then
+     do stuff for user 1000
+else
+     do stuff for other users
+fi
